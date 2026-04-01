56fcde0c1323625abf37e2cd868de3051eb6c7d6
Hady <srhady12@gmail.com>
2026-04-01 08:42:03 +0600

    Update scraper.sh
    Signed-off-by: Hady <srhady12@gmail.com>

--- scraper.sh
+++ scraper.sh
@@ -7,7 +7,7 @@ OFFSET=0
 LIMIT=100
 FIRST_ITEM=true
 
-# আপনার দেওয়া টোকেন
+# আপনার টোকেন এখানে দেবেন
 AUTH_TOKEN="eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJDVUtWUkdUMFp3VEp3VEFfMDVPVXBwV1NwTzN6bzE5aUphVHJwdk9MTkpjIn0.eyJleHAiOjE3NzUwMTI2NzIsImlhdCI6MTc3NTAwOTA3MiwianRpIjoib25ydGNjOjVhMmExYTQwLWI5NDQtMzA5ZC05NzdkLWE3YzExZWY4MDhlMiIsImlzcyI6Imh0dHBzOi8vYWNjb3VudHMuYm9uZ29iZC5jb20vcmVhbG1zL2JvbmdvIiwic3ViIjoiYzY0ZTA3MjgtNjNjMy00OGFkLWIxOTAtZjM0MzhkOTVjYzI0IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYW5vbnltb3VzIiwic2lkIjoiNTI2ZDg2ZWItYTE5NC00OTUyLWI2OGYtYTI3ZDAyYmQ5YzVlIiwic2NvcGUiOiIiLCJjb3VudHJ5X2NvZGUiOiIiLCJ1c2VyX3R5cGUiOiJhbm9ueW1vdXMiLCJjbGllbnRIb3N0IjoiMTAzLjE3NC4xOTIuNTciLCJwbGF0Zm9ybV9pZCI6ImFiZmVhNDYyLWY2NGQtNDkxZS05Y2Q5LTc1ZWUwMDFmNDViMCIsImJvbmdvX2lkIjoiMjM4ZTdlNjMtN2UwNS00OGM0LThjM2YtMWE5NmM5ZmUzMzgyIiwiY2xpZW50QWRkcmVzcyI6IjEwMy4xNzQuMTkyLjU3IiwiY2xpZW50X2lkIjoiYW5vbnltb3VzIiwidXNlcm5hbWUiOiJhbm9ueW1vdXMifQ.fZAsFWOC7vft_nliYuZqkoqcBp-cyRks2Yahk3FrndrC2XXO0w5Hj1zCT-6jcgHRIUzbv_LnqK-dq_weaBxskSKSkr5nfrl_Mw9nLWH9TmK8YDSZ3hQBsZuao8gwqNowwZRjL0FCHFOnzhXZgO1Yie7ecXTGFxM-lsWjsX1mncPWBhHI7EZL27Pn8V6zgPraRanW1FCLqUHIZ2AfojZakKKtBTYwf0NIIzrfV37w4O1RWpBvuuBqpVJlX40Hq_9hMnSAydxtWXweUwXpj_mmlxD5tGPUHRQJgmmWW_YiMp5R5nFIw1ngMAppqs_zSqFwiRI0gaz6IWZJI5Y7y0DAy2zcG_QtOr3xxCSanV9Tx1P8f_g7jWD__wsEY76Yk4sF6Iux3fRPVenWf0AfbtYwUhg_xZxvAbIDNXqB6hHknoHHSBCV_5mHO0DtXZKTP_AHu8si-hJkr8zxwt3r-CfEj2I_S7PiKZ3knqmas-zblWYMwIJc7ppmDna0K_hxFYpmRPWHtzclH7aPFPcTplXQqzY7kteKRngHypdU0svsRoGVcihTF-PtXr7Boom6XUj3kdEsN97Ymo4_nkQjBOZAHp7Z4xU8A7agCquoVu3O8UaTOLnAq0XT_mk-CiAYSNrRtBgPzSQviYsQsWtNRxjJ2R9U4k8qP8cbiFZicgbOFOk"
 
 echo "🚀 Starting GitHub Actions Scraper..."
@@ -46,7 +46,8 @@ while true; do
       echo "," >> $OUTPUT_FILE
     fi
     
-    echo "$DETAIL" >> $OUTPUT_FILE
+    # ম্যাজিক লাইন: ফাইলে ঢোকানোর আগে jq . দিয়ে ডেটা সাজিয়ে নেওয়া হচ্ছে
+    echo "$DETAIL" | jq . >> $OUTPUT_FILE
     sleep 1
   done
 
